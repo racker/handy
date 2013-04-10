ea5156614ffe7d7875a2c2d436ccdc83eeadca8e
Malini Kamalambal <malini.pk@gmail.com>
2013-04-10 12:51:53 -0400

    Added tests for Queue stats

--- queue/queuefnlib.py
+++ queue/queuefnlib.py
@@ -1,3 +1,5 @@
+import json
+
 def verifymetadata(getdata, postedbody):
     """@todo - Really verify the metadata"""
     testresultflag = False
@@ -9,3 +11,31 @@ def verifymetadata(getdata, postedbody):
         print("AYYY")
     else:
         print("NAYYY")
+
+def verifyqueuestats(*getresponse):
+    testresultflag = True
+    headers = getresponse[0]
+    body = json.loads(getresponse[1])
+    keysinbody = body.keys()
+    keysinbody.sort()
+    if  (keysinbody == ["actions", "messages"]):
+        stats = body["messages"]
+        keysinstats = stats.keys()
+        keysinstats.sort()
+        if (keysinstats == ["claimed", "expired", "total"]) :
+            try:
+                int(stats["claimed"])
+                int(stats["expired"])
+                int(stats["total"])
+            except:
+                testresultflag = False
+        else:
+            testresultflag = False
+    else:
+        testresultflag = False
+    if testresultflag:
+        return testresultflag
+    else:
+        print headers
+        print body
+        assert testresultflag, "Get Request stats failed"
